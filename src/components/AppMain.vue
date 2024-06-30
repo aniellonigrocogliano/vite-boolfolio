864447bd24ccbfa5192a9eabe63894c5714456f2
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-06-30 17:32:55 +0200

    add: card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,14 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 export default {
+    components: {
+        ProjectCard,
+
+    },
     data() {
         return {
-            project: null,
+            project: [],
         };
     },
     created() {
@@ -15,17 +20,22 @@ export default {
                 .get('http://127.0.0.1:8000/api/projects')
                 .then((resp) => {
                     console.log(resp);
-                    this.project = resp.data.response;
+                    this.project = resp.data.results;
                 })
                 .catch((error) => {
                     console.error(error);
                 });
-            console.log(this.project);
         },
     },
 };
 </script>
 <template>
-
+    <div class="container">
+        <div class="row row-cols-4">
+            <div class="col" v-for="card in project.data">
+                <ProjectCard :cardObj="card" />
+            </div>
+        </div>
+    </div>
 </template>
 <style lang="scss"></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    props: {
+        cardObj: Object,
+    }
+};
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+
+        <div class="card-body">
+            <h5 class="card-title">{{ cardObj.title }}</h5>
+            <p class="card-text">Descrizione: {{ cardObj.description }}</p>
+            <p class="card-text">Autore: {{ cardObj.author }}</p>
+            <p class="card-text">Data crerazione {{ cardObj.creation_date }}</p>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
